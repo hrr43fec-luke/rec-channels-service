eebbc8e9d6e17349b7a4bff01bfd938b2e6440c4
azdoolittle <drewdoolittle@gmail.com>
2020-01-27 15:10:14 -0800

    airbnb refactor for db

--- db/index.js
+++ db/index.js
@@ -1,14 +1,14 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/channels', {useNewUrlParser: true});
-var channelSchema = require('./schema.js');
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://localhost/channels', { useNewUrlParser: true });
+const channelSchema = require('./schema.js');
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'db connection error:'));
-db.once('open', ()=> {
-  console.log.bind(console, 'db connected!')
+db.once('open', () => {
+  console.log.bind(console, 'db connected!');
 });
 
-var Channel = mongoose.model('Channel', channelSchema);
+const Channel = mongoose.model('Channel', channelSchema);
 
 module.exports = Channel;
--- db/schema.js
+++ db/schema.js
@@ -1,14 +1,14 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var channelSchema = new mongoose.Schema({
-  "id": Number,
-  "user_id": Number,
-  "user_name": String,
-  "game_id": Number,
-  "live": Boolean,
-  "title": String,
-  "viewer_count": Number,
-  "thumbnail_url": String
+const channelSchema = new mongoose.Schema({
+  id: Number,
+  user_id: Number,
+  user_name: String,
+  game_id: Number,
+  live: Boolean,
+  title: String,
+  viewer_count: Number,
+  thumbnail_url: String,
 });
 
 module.exports = channelSchema;
