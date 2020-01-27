53db99243a6bc04ee612b4fcd3c16f6d391117a8
azdoolittle <drewdoolittle@gmail.com>
2020-01-27 12:23:11 -0800

    isolated db/index.js from schema

--- db/index.js
+++ db/index.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/channels', {useNewUrlParser: true});
+var channelSchema = require('./schema.js');
+
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'db connection error:'));
@@ -7,26 +9,6 @@ db.once('open', ()=> {
   console.log.bind(console, 'db connected!')
 });
 
-var channelSchema = new mongoose.Schema({
-  "id": Number,
-  "user_id": Number,
-  "user_name": String,
-  "game_id": Number,
-  "live": Boolean,
-  "title": String,
-  "viewer_count": Number,
-  "thumbnail_url": String
-});
-
 var Channel = mongoose.model('Channel', channelSchema);
 
-var emongg = new Channel({
-  "id": 12345,
-  "user_id": 54321,
-  "user_name": 'Emongg',
-  "game_id": 55555,
-  "live": true,
-  "title": 'Road to GM',
-  "viewer_count": 9999,
-  "thumbnail_url": 'https://s3.amazonaws.com/uifaces/faces/twitter/iamjdeleon/128.jpg'
-})
+module.exports = Channel;
