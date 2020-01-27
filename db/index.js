a922caa1dfad2b1bfcf12745ebba45b68e999201
azdoolittle <drewdoolittle@gmail.com>
2020-01-27 10:16:47 -0800

    channelSchema

--- db/index.js
+++ db/index.js
@@ -6,3 +6,14 @@ db.on('error', console.error.bind(console, 'db connection error:'));
 db.once('open', ()=> {
   console.log.bind(console, 'db connected!')
 });
+
+var channelSchema = new mongoose.Schema({
+  "id": Number,
+  "user_id": Number,
+  "user_name": String,
+  "game_id": Number,
+  "live": Boolean,
+  "title": String,
+  "viewer_count": Number,
+  "thumbnail_url": String
+});
