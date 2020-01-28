928d230e70e03da98a3a8378c52be697bead0291
azdoolittle <drewdoolittle@gmail.com>
2020-01-28 14:03:00 -0800

    GET req now only returns channels where live: true

--- db/dbHelpers.js
+++ db/dbHelpers.js
@@ -2,7 +2,7 @@ const Channel = require('./index');
 
 const helper = {
   getChannels: (callback) => {
-    Channel.find(callback).sort({ viewer_count: -1 }).limit(10);
+    Channel.find({ live: { $eq: true } }, callback).sort({ viewer_count: -1 }).limit(10);
     console.log('db getChannels cb===', callback);
   }
 };
