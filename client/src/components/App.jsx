6bc56c46203a5f20283e7caac397d54db89c4982
azdoolittle <drewdoolittle@gmail.com>
2020-01-28 12:52:16 -0800

    props correctly rendering after db fetch

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,34 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import ChannelList from './ChannelList.jsx';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    this.state.channels = [];
+    console.log('state:', this.state.channels)
+  }
+
+  componentDidMount() {
+    fetch('/channels')
+      .then((response) => {
+        return response.json()
+          .then((body) => {
+            console.log('body', body)
+            this.setState({ channels: body });
+          });
+      });
   }
 
   render() {
+    const { channels } = this.state.channels;
+    console.log('channels:', channels)
     return (
       <div className="helloWorld">
         <h1>
           `Hello World! It's me, the Recommended Channels Module`
+          <ChannelList channels={this.state.channels}/>
         </h1>
       </div>
     );
