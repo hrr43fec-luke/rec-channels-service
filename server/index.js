de221c23829ee7fde6dc467229297e7040775fc1
azdoolittle <drewdoolittle@gmail.com>
2020-01-28 18:43:24 -0800

    port number changed to 3004 to reflect module order

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = require('./router');
 
 const app = express();
-const port = 3000;
+const port = 3004;
 app.use(router);
 app.use(express.static('public'));
 
